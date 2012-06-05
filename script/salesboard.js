83535aceeec9a39035308e63a524c5303b3d7891
Daan Kroese <daankroese@gmail.com>
2012-06-05 12:39:16 +0200

    Catch invalid input, don't send it to data handler

--- script/salesboard.js
+++ script/salesboard.js
@@ -82,12 +82,17 @@ function saveData(event)
 {
   if (typeof event.keyCode == 'undefined' || event.keyCode == 13)
   {
-    // Send new data to data handler
+    // Get new data from cell
     //console.log(this, event);
     var input = $(this);
     var value = parseInt(input.val(), 10);
-    var id = input.parent().attr('id').split('-');
-    $.post('data.php', {"action":"set", "data":{"id":id, "value":value}}, dataSaved);
+
+    // If data is valid number, send it to the data handler
+    if (!isNaN(value))
+    {
+      var id = input.parent().attr('id').split('-');
+      $.post('data.php', {"action":"set", "data":{"id":id, "value":value}}, dataSaved);
+    }
 
     // Update view back to normal
     input.parent().html(value);
